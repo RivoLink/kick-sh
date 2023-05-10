41a2d69dcfdd9c2e776b45ade57b3d4f8ceb091c
RivoLink <rivo.link@gmail.com>
2023-05-10 21:01:26 +0300

    add udpkg.sh

---
+++ udpkg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# about: unlock dpkg
+
+# kill apt process
+sudo killall apt apt-get | yes
+
+# remove the lock files
+sudo rm -rf /var/lib/apt/lists/lock
+sudo rm -rf /var/cache/apt/archives/lock
+sudo rm -rf /var/lib/dpkg/lock*
+
+# reconfigure the packages
+sudo dpkg --configure -a
+
+# update and upgrade
+sudo apt update && sudo apt upgrade
+
