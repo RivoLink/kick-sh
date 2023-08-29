d50e18e56eee045b18e7045f71d4a610553d1115
Rivo Link <rivo.link@gmail.com>
2023-08-29 15:05:34 +0300

    update v2.1 devmode.sh

--- devmode.sh
+++ devmode.sh
@@ -6,7 +6,33 @@ sudo systemctl start mysql
 # start phpMyAdmin
 sudo systemctl start rl-phpmyadmin
 
-# start docker
-if [ "$1" == "--docker" ]; then
-      sudo systemctl start docker
-fi
+# func to start docker
+start_docker() {
+    sudo systemctl start docker
+}
+
+# func to display help
+display_help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -d, --docker   Start Docker"
+    echo "  -h, --help     Display this help message"
+}
+
+# parse options
+for arg in "$@"; do
+    case $arg in
+        --docker | -d)
+            start_docker
+            ;;
+        --help | -h)
+            display_help
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $arg"
+            echo "Display help with: $0 --help"
+            exit 1
+            ;;
+    esac
+done
